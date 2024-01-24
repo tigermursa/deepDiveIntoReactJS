bfcd0b276a32bbe4284fb75ffd39522beac9bd6d
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-24 08:43:57 +0600

    "class seven finished"

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { easeIn } from "framer-motion/dom";
 
 const App = () => {
-  const initial = { rotate: 0 };
-  const animate = { rotate: 360 };
+  const parent = {
+    hidden: { x: 0, y: 0 },
+    visible: {
+      x: [50, 100],
+      y: [-50, -100],
+      scale: 1,
+      transition: {
+        ease: "linear",
+        duration: 1,
+        repeat: Infinity,
+      },
+    },
+  };
 
   return (
-    <div>
-      <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen cursor-grabbing ">
+      <div className="border border-blue-800">
         <motion.div
-          className="h-20 w-20 bg-red-600 rounded-md"
-          initial={initial}
-          animate={animate}
-          transition={{ type: "spring", duration: 2 }}
+          className="h-20 w-20 bg-red-600 rounded-md  "
+          variants={parent}
+          initial={"hidden"}
+          animate={"visible"}
+          whileHover={{
+            scale: 1.1,
+          }}
+          whileTap={{ rotate: 45 }}
+          drag
+          dragConstraints={{ left: 50, right: 50, top: -150, bottom: 150 }}
+          dragSnapToOrigin
         ></motion.div>
       </div>
     </div>
