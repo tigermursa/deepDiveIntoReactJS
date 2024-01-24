8ec05e582480c4a12f0838b6fb0c11898bcecd88
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-24 07:29:13 +0600

    "installed framer motion"

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
   return (
     <div>
-      <p>hi</p>
+      <div className="flex justify-center items-center h-screen">
+        <motion.div className="h-20 w-20 bg-red-600 rounded-full  "></motion.div>
+      </div>
     </div>
   );
 };
