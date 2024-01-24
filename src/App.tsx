e93a2f9166916aff26ba36c1c2004b8d2e27deab
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-24 07:38:33 +0600

    "first class done "

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const App = () => {
+  const initial = { rotate: 0 };
+  const animate = { rotate: 360 };
+
   return (
     <div>
       <div className="flex justify-center items-center h-screen">
-        <motion.div className="h-20 w-20 bg-red-600 rounded-full  "></motion.div>
+        <motion.div
+          className="h-20 w-20 bg-red-600 rounded-md"
+          initial={initial}
+          animate={animate}
+          transition={{ type: "spring", duration: 2 }}
+        ></motion.div>
       </div>
     </div>
   );
