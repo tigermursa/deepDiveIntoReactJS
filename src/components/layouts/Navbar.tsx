12a342fa543aefc0f089657199cabc5b47507e0d
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-25 08:07:19 +0600

    "navbar structure done"

---
+++ src/components/layouts/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Button } from "../ui/button";
+
+const Navbar = () => {
+  return (
+    <header className="h-16">
+      <nav className="w-full h-full max-w-[1220px] px-[20px] mx-auto flex justify-between items-center bg-green-400 ">
+        <span className="text-3xl">iRepair</span>
+        <ul className="space-x-5 font-semibold">
+          <NavLink to={"/"}>Home</NavLink>
+          <NavLink to={"/about"}>About</NavLink>
+          <NavLink to={"/service"}>Service</NavLink>
+          <Button>
+            <NavLink to={"/login"}>Login</NavLink>
+          </Button>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -10,7 +10,7 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "/",
+        index: true,
         element: <Home />,
       },
       {
