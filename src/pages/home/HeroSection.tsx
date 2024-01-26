c68f1d277d071ef724f6930128b3d99996703f9d
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-26 22:15:15 +0600

    "animation done for text "

--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -1,27 +1,62 @@
 import Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
 import tab from "../../assets/Samsung_Galaxy_Tab_S3.png";
-import React from "react";
+import { motion } from "framer-motion";
+
+const intro = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.25,
+      delayChildren: 1,
+    },
+  },
+};
+
+const introChildren = {
+  hidden: { opacity: 0, y: -200 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.7,
+      type: "spring",
+      bounce: 0.5,
+    },
+  },
+};
+
+const tabAnimation =P
 
 const HeroSection = () => {
   return (
-    <Container className="h-[calc(100vh-64px)] grid grid-cols-2 place-content-center ">
-      <div>
-        <h1 className="text-6xl font-bold">
+    <Container className="h-[calc(100vh-64px)] grid grid-cols-1 lg:grid-cols-2 place-content-center ">
+      <motion.div variants={intro} initial="hidden" animate="visible">
+        <motion.h1
+          className="text-6xl font-bold text-nowrap"
+          variants={introChildren}
+        >
           <span className="text-gray mb-10">Hey Nigga</span>
           <br></br>
           <span className="">Buy Buy</span>
-        </h1>
-        <p className=" text-dark-gray max-w-[50ch] mt-10 mb-5 text-lg">
+        </motion.h1>
+        <motion.p
+          className=" text-dark-gray max-w-[50ch] mt-10 mb-5 text-lg"
+          variants={introChildren}
+        >
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
           explicabo quisquam quidem voluptatibus magni ullam nam debitis
           laudantium deleniti doloremque.
-        </p>
-        <Button>Book Service Now</Button>
-      </div>
-      <div className=" flex justify-center">
+        </motion.p>
+        <motion.div variants={introChildren}>
+          <Button>Book Service Now</Button>
+        </motion.div>
+      </motion.div>
+      <div className=" flex justify-center mt-10 w-3/4 lg:w-full mx-auto">
         <img
-          className="-rotate-[30deg]"
+          className="-rotate-[35deg] h-[95%] object-contain"
           width={350}
           height={350}
           src={tab}
