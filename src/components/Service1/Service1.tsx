38d9a3cbb2c91b42b1a24caa290e59202ecc86e8
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-27 18:56:21 +0600

    "added the the animation in a hook"

--- src/components/Service1/Service1.tsx
+++ src/components/Service1/Service1.tsx
@@ -1,23 +1,13 @@
 import { useScroll, motion, useTransform } from "framer-motion";
 import { useRef } from "react";
+import useScrollGrow from "../hooks/useScrollGrow";
 
 const Service1 = () => {
-  const componentRef = useRef<HTMLDivElement>(null);
+  const { componentRef, style } = useScrollGrow();
 
-  const { scrollYProgress } = useScroll({
-    target: componentRef,
-    offset: ["0 1", "2 1"],
-  });
-  const scaleValues = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
-  //const opacityValues = useTransform(scrollYProgress, [0, 1], [0.2, 1]);
-  //const xValues = useTransform(scrollYProgress, [0, 1], [-1000, 0]);
   return (
     <motion.div
-      style={{
-        scale: scaleValues,
-       // opacity: opacityValues,
-       // x: xValues,
-      }}
+      style={style}
       ref={componentRef}
       className="bg-red-500 h-[415px] rounded-2xl col-span-12"
     ></motion.div>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,9 +3,15 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <Container className="">This is About</Container>
-    </div>
+    <Container className="pt-16">
+      <div>
+        <h1>This is about section</h1>
+        <h2>This is about section</h2>
+        <h3>This is about section</h3>
+        <h4>This is about section</h4>
+        <h5>This is about section</h5>
+      </div>
+    </Container>
   );
 };
 
