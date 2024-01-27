cb09cabb46520723111118387d0e956b22705fac
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-27 21:35:47 +0600

    "done class for today"

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,7 +6,6 @@ const About = () => {
     <Container className="pt-16">
       <div>
         <h1>This is about section</h1>
-        <h2>This is about section</h2>
         <h3>This is about section</h3>
         <h4>This is about section</h4>
         <h5>This is about section</h5>
