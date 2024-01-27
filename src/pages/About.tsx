a79a98cd1d8f27fcad20c580c81cf2307d907108
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-27 23:32:34 +0600

    "going to start class 09"

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,8 +7,7 @@ const About = () => {
       <div>
         <h1>This is about section</h1>
         <h3>This is about section</h3>
-        <h4>This is about section</h4>
-        <h5>This is about section</h5>
+        
       </div>
     </Container>
   );
