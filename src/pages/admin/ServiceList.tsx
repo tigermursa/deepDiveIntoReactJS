5e939374f1ced8099f9142cbe9fadbe8f34b3df7
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-28 11:40:10 +0600

    "added table from shadcn"

--- src/pages/admin/ServiceList.tsx
+++ src/pages/admin/ServiceList.tsx
@@ -1,8 +1,16 @@
-
-
 import { getAllUsers } from "@/api/admin/users/users.api";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { useQuery } from "@tanstack/react-query";
-
 
 const ServiceList = () => {
   //   const [data, setData] = useState([]);
@@ -28,16 +36,50 @@ const ServiceList = () => {
     return <h2>Error fetching data</h2>;
   }
 
+  //   <div>
+  //   {data?.map((user) => (
+  //     <div key={user.id} className="p-4 border">
+  //       <h5>Person id:{user.id}</h5>
+  //       <p>Name:{user.name}</p>
+  //       <p>City:{user.address.city}</p>
+  //     </div>
+  //   ))}
+  // </div>
+
   return (
-    <div>
-      {data?.map((user) => (
-        <div key={user.id} className="p-4 border">
-          <h5>Person id:{user.id}</h5>
-          <p>Name:{user.name}</p>
-          <p>City:{user.address.city}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Id</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead className="">Phone</TableHead>
+            <TableHead className="text-right">Action</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.map((user) => (
+            <TableRow key={user.id}>
+              <TableCell className="font-medium">{user.id}</TableCell>
+              <TableCell className="font-medium">{user.name}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.phone}</TableCell>
+
+              <TableCell className="text-right">
+                <Button variant={"outline"}>Hello</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={3}>Total</TableCell>
+            <TableCell className="text-right">$2,500.00</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </>
   );
 };
 
