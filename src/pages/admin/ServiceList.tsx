eaf6cffb32883145149953a8894b8c14c0378827
Mursalin Hossain <mursalin2311@gmail.com>
2024-01-28 10:19:19 +0600

    "used normal useEffect hook to revision"

--- src/pages/admin/ServiceList.tsx
+++ src/pages/admin/ServiceList.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const ServiceList = () => {
-    return (
-        <div>
-            <p>yo yo services</p>
+//   const [data, setData] = useState([]);
+//   useEffect(() => {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((res) => res.json())
+//       .then((data) => setData(data));
+//   }, []);
+
+  return (
+    <div>
+      {data.map((user) => (
+        <div key={user.id} className="p-4 border">
+          <h5>Person id:{user.id}</h5>
+          <p>Name:{user.name}</p>
+          <p>City:{user.address.city}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default ServiceList;
